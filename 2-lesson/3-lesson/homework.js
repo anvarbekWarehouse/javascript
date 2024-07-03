63323db7e0fde9357de6195810b3c375944da68f
Anvarbek Turdaliyev <a.turdaliev@hamkorbank.uz>
2024-07-03 13:49:57 +0500

    switch operators

---
+++ 2-lesson/3-lesson/homework.js
@@ -0,0 +1,17 @@
+// let a = 12;
+// if (a % 2 == 0 ) console.log("Juft son");
+// else console.log("Toq son");
+
+// let a = "son";
+
+// if( s === "Son") console.log(true);
+// else console.log(false);
+
+// Ternary opertaor
+
+let number = 100;
+
+number < 200 ? console.log("Kichik") : console.log("katta");
+
+console.log(0 || 1);
+console.log(null ?? 1);
---
+++ 4-lesson/index.js
@@ -0,0 +1,20 @@
+// <!-- if -> <, >, <=, >=, ==, ===, !=, !==, ... -->
+// <!-- switch -> === -->
+
+let data = "bulut";
+
+switch(data){
+    case 1:
+        console.log("teng");
+        break;
+    case "bulut":
+        console.log("Bugun bulutli");
+        break;
+    case "qor":
+        console.log("bugun qor");
+        break;
+    default:
+        console.log("bunday qiymat mavjud emas!")
+}
+
+
